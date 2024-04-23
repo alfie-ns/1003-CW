4360f2861d6544367b378b6eba80e97b36bb5377
alfie-ns <alfienurse@gmail.com>
2024-04-23 12:22:43 +0100

    update

--- Report/AVL/Solution/VS/BinarySearchTree/BinarySearchTree/Program.cs
+++ Report/AVL/Solution/VS/BinarySearchTree/BinarySearchTree/Program.cs
@@ -567,7 +567,7 @@ class Program // Program class, the entry point of the program
     {
         Tree tree = new Tree(); // init test tree
 
-        int[] largeDataSet = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50}; // init large dataset
+        int[] largeDataSet = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50}; // init large-ish dataset
 
         foreach (int element in largeDataSet) // for EVERY element in largeDataSet array
         {
